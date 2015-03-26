53445cce6f7580e06ee0e64ff7f559eb6b996cb1
Jon Bloom <git@jon.tw>
2015-03-26 11:47:47 -0400

    forget pyaudio

---
+++ beep.py
@@ -0,0 +1,3 @@
+from room import beep
+beep()
+exit()
--- room.py
+++ room.py
@@ -2,47 +2,40 @@ import urllib2
 import json
 from datetime import datetime
 from time import sleep
-import wave, sys, pyaudio
-
+from subprocess import call
 BASE_URL = 'http://labs.library.gvsu.edu/display/getRoomAvailability.php?roomId='
+MINUTES = 10
 
 def checkRoom(id):
 	results = urllib2.urlopen(BASE_URL + str(id)).read()
+	print (chr(27) + "[2J")		
 	print results
-	reservation = json.loads(results)
-	reservation_id = reservation['ReservationId']['0']
-	end = reservation['TimeEnd'].split(':')
-	now = datetime.now()
-	end = datetime(now.year,now.month,now.day,int(end[0]),int(end[1]))
-	remaining = (end-now).seconds/60
+	try:
+		reservation = json.loads(results)
+		reservation_id = reservation['ReservationId']['0']
+		end = reservation['TimeEnd'].split(':')
+		now = datetime.now()
+		end = datetime(now.year,now.month,now.day,int(end[0]),int(end[1]))
+		remaining = (end-now).seconds/60
+	except:
+		pass
 	return remaining, reservation_id
 
 def beep():
-	wf = wave.open('beep.wav')
-	p = pyaudio.PyAudio()
-	chunk = 1024
-	stream = p.open(format =
-	                p.get_format_from_width(wf.getsampwidth()),
-	                channels = wf.getnchannels(),
-	                rate = wf.getframerate(),
-	                output = True)
-	data = wf.readframes(chunk)
-	while data != '':
-	    stream.write(data)
-	    data = wf.readframes(chunk)
+	call('aplay /home/pi/reminder/warning.wav', shell=True)
 def main():
-	with open('roomid.txt') as f:
+	with open('/home/pi/reminder/roomid.txt') as f:
 		room_id = int(f.read())
 	last_id = 0
 	while True:
 		try:
-			minutes, reservation_id = checkRoom(room_id)
-			if minutes < 15 and reservation_id != last_id:
+			minutes_left, reservation_id = checkRoom(room_id)
+			if minutes_left < MINUTES and reservation_id != last_id:
 				print reservation_id
 				last_id = reservation_id
 				beep()
 			else:
-				print minutes, reservation_id
+				print minutes_left, reservation_id
 			sleep(10)
 		except KeyboardInterrupt:
 			exit()
