1b372ca06d5f171177e2027c6f8b3a9033c63d63
Jon Bloom <git@jon.tw>
2015-03-31 13:22:58 -0400

    only warn after the reserver changes

--- room.py
+++ room.py
@@ -1,43 +1,70 @@
 import urllib2
 import json
+import xml.etree.ElementTree as ET
 from datetime import datetime
+import time
 from time import sleep
 from subprocess import call
 from os import getcwd
-BASE_URL = 'http://labs.library.gvsu.edu/display/getRoomAvailability.php?roomId='
+from collections import defaultdict
+
+BASE_URL = 'http://gvsu.edu/reserve/files/cfc/functions.cfc?method=bookings&roomId={0}&startDate={1}&endDate={2}'
 MINUTES = 10
 PATH = getcwd() + '/'
+SOUND = 'warning.wav'
 
-def checkRoom(id):
-	results = urllib2.urlopen(BASE_URL + str(id)).read()
-	print (chr(27) + "[2J")		
-	print results
-	try:
-		reservation = json.loads(results)
-		reservation_id = reservation['ReservationId']['0']
-		end = reservation['TimeEnd'].split(':')
-		now = datetime.now()
-		end = datetime(now.year,now.month,now.day,int(end[0]),int(end[1]))
-		remaining = (end-now).seconds/60
-	except:
-		pass
-	return remaining, reservation_id
 
-def beep():
-	call('aplay ' + PATH + 'warning.wav', shell=True)
+def get_info_from_booking(booking):
+	now = datetime.now()
+	data = defaultdict(str)
+	data['booking_id'] = booking.find('BookingID').text
+	data['start'] = booking.start_time
+	data['end'] = booking.end_time
+	data['minutes_left'] = booking.end_time.minute - now.minute 
+	data['reserved_by'] = booking.find('EventName').text.split(' - ')[0]
+	return data
+
+def get_room(room_id):
+	now = datetime.now()
+	now_str = now.strftime('%Y-%m-%d')
+	url = BASE_URL.format(*[room_id,now_str,now_str])
+	results = urllib2.urlopen(url).read()
+
+	xml = ET.fromstring(results)
+	bookings = sorted(xml.findall('Data'),key=get_time)
+	current = None
+	
+	for booking in bookings:
+		booking.start_time = datetime.strptime(booking.find('TimeEventStart').text, '%Y-%m-%dT%H:%M:%S')
+		booking.end_time = datetime.strptime(booking.find('TimeEventEnd').text, '%Y-%m-%dT%H:%M:%S')	
+		if booking.start_time < now < booking.end_time:
+			current = booking
+	if current is not None:
+		return get_info_from_booking(current) 
+
+
+def get_time(booking):
+	date = datetime.strptime(booking.find('TimeEventStart').text,'%Y-%m-%dT%H:%M:%S')
+	return date
+
+def play_sound(sound):
+	call('aplay ' + PATH + sound, shell=True)
+
 def main():
 	with open(PATH + 'roomid.txt') as f:
 		room_id = int(f.read())
-	last_id = 0
+	last_id = None
+	last_reserved_by = None
+	
 	while True:
 		try:
-			minutes_left, reservation_id = checkRoom(room_id)
-			if minutes_left < MINUTES and reservation_id != last_id:
-				print reservation_id
-				last_id = reservation_id
-				beep()
+			room = get_room(room_id)
+			if room['minutes_left'] < MINUTES and room['booking_id'] != last_id and room['reserved_by'] != last_reserved_by:
+				last_id = room['booking_id']
+				last_reserved_by = room['reserved_by']
+				play_sound(SOUND)
 			else:
-				print minutes_left, reservation_id
+				print room['minutes_left']
 			sleep(10)
 		except KeyboardInterrupt:
 			exit()
